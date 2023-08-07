33c8a8eb94228d0650ea7a56e12013ff0ad7ee34
Satya5495 <satyanarayan.patra5495@gmail.com>
2023-08-07 11:10:23 +0530

    updated codes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
             }}
         >
             <html lang='en'>
-                <body className={`${inter.className} bg-dark-1`}>{children}</body>
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full min-h-screen flex justify-center items-center">
+                        {children}
+                    </div>
+                </body>
             </html>
         </ClerkProvider>
     );
